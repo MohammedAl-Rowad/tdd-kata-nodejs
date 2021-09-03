b13a0778deca3836503ba152de7c8cf53334a699
MohammedAl-rowad <rowadwo@gmail.com>
2021-09-03 20:12:41 +0300

    Implement obstacles detection

--- src/controllers/commands.controller.js
+++ src/controllers/commands.controller.js
@@ -15,7 +15,7 @@ const {
  * @param {ExprResponse} res
  */
 const handleCommand = (req, res) => {
-  const { commands, x, y, direction } = req.body
+  const { commands, x, y, direction, obstacles } = req.body
 
   if (!commands || !x || !y || !direction) {
     res.sendStatus(400)
@@ -25,12 +25,25 @@ const handleCommand = (req, res) => {
     let resX = x
     let resY = y
     let resDir = direction
+    let obstaclesMap = {}
+    if (obstacles && obstacles.length) {
+      obstaclesMap = obstacles.reduce(
+        (prev, [oX, oY]) => ({ ...prev, [`${oX}:${oY}`]: true }),
+        {}
+      )
+    }
 
     for (const c of commands) {
       if (c === 'F' || c === 'B') {
         const [toX, toY] = DIRECTIONS[resDir]
         resX += c === 'B' ? -1 * toX : toX
         resY += c === 'B' ? -1 * toY : toY
+        const key = `${resX}:${resY}`
+        if (key in obstaclesMap) {
+          resX -= c === 'B' ? -1 * toX : toX
+          resY -= c === 'B' ? -1 * toY : toY
+          res.send({ x: resX, y: resY, direction: resDir, status: 'STOPPED' })
+        }
       } else {
         resDir = calcDirection(resDir)(c)
       }
